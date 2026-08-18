a63067f2417a154432b274c4fce2c6f6268ae66a
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-18 15:41:06 +0000

    chore: remove unused authentication middleware configuration

--- babi_flutter_web/android/app/src/main/kotlin/com/example/babi_flutter_web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.babi_flutter_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- babi_flutter_web/lib/core/providers/product_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/dummy_products.dart';
-
-final productsProvider = FutureProvider<List<Map<String, dynamic>>>((ref) async {
-  // Fournit la liste complète des produits enregistrés de la boulangerie
-  return ProductData.products;
-});
