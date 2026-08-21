b1dda4b5fc033582bde75d20b19d87c3b6e4005f
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-21 16:31:30 +0000

    style: restore classic brand croissant emblem logo and typography on auth screens

--- Boulangerie-de-BABI-main/app/flutter_bootstrap.js
+++ Boulangerie-de-BABI-main/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3006059265" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "951386461" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
