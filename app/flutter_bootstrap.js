55f4f8d6d4d2682ac157d3b0f3bf91b745aeff8d
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-21 06:21:12 +0000

    feat(auth): use official multi-color Google vector logo on login and register screens

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1541289475" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3705464341" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
