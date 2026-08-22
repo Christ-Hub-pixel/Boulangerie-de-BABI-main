a931e92bb2bb761771713ff071939e6bc40af18c
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-22 04:47:09 +0000

    Profil, Babi Club, Fournil Favori, Wave, Bons d'achat et reglages 100% reels et fonctionnels

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "803479194" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3029749818" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
