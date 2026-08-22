74731a08fd2a1797cf50c0c5bf7a4c4ee70f6a51
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-22 06:07:38 +0000

    Correction complete des photos manquantes du catalogue avec ProductImageView et normalisation WebP

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1424983498" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1530542176" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
