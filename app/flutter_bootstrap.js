ab7cddbcace9ad725b3bf47c463f98327da50405
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-22 06:27:55 +0000

    feat: Realtime order tracking, live badges, PIN security and persistent order management

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1530542176" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1855963853" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
