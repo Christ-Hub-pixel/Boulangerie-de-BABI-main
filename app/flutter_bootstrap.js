fa53ce169ae24c99dc2858f22d532da31d09172e
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-22 05:48:31 +0000

    Suppression du bouton de simulation Etape suivante sur l ecran de suivi de commande

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2161872676" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1424983498" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
