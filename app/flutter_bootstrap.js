81d9abc7ecd6cd6292a561f3644055de9b35ccde
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-22 04:30:33 +0000

    Profil utilisateur 100% reel : persistance SharedPreferences/Firebase/API, mise a jour dynamique, avatar Google interactif

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2583168538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "803479194" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
