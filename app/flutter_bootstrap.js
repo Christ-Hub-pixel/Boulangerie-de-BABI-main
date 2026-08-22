63693a17a343a8512524e1da0ac754b6799b94fc
Christ-Hub-pixel <christezoua57@gmail.com>
2026-08-22 05:00:36 +0000

    Correction commande panier et integration fluide Wave, codes promo et choix boutique

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3029749818" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3574748263" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
